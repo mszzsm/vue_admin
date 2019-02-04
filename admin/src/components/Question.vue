da09ea8b8abbb4255f93944d773c096bcd76a681
Michał Sztefanica <mikeshtefanitsa@gmail.com>
2019-02-04 23:57:47 +0100

    calculations

--- admin/src/components/Question.vue
+++ admin/src/components/Question.vue
@@ -1,10 +1,44 @@
 <template>
-    <h2>Question</h2>
+   <div class="alert alert-secondary">
+       <h3>{{ x }} + {{ y }} = {{ answers }}</h3>
+       <hr>
+       <div class="buttons">
+           <button class="btn btn-success" v-for="(number, key, index) in answers">
+               {{number}}
+           </button>
+       </div>
+   </div>
 </template>
 <script>
 
 export default {
-    
+    data() {
+        return {
+            x: mtRand(100, 200),
+            y: mtRand(100, 200)
+        }
+    },
+    computed: {
+        answers() {
+            var good = [this.x + this.y];
+            var res  = good;
+
+            while(res.length < 2){
+                var num = mtRand(good - 20, good + 20);
+                if (res.indexOf(num) === -1){
+                res.push(num);
+                }
+            }
+            return res.sort(function(){
+                return Math.random() > 0.9;
+            })
+        }
+    },
+}
+
+function mtRand(min, max) {
+    let diff = max - min;
+    return Math.floor(Math.random() * (diff+1)) + min;
 }
 </script>
 
